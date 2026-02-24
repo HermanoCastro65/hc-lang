82973d5e095b3e9231302299d008e35759d349cd
HermanoCastro65 <hermanocastro18@gmail.com>
2026-02-24 16:01:49 -0300

    feat(cli): add --all option to run all examples and clean generated files

--- bin/hc.lua
+++ bin/hc.lua
@@ -1,61 +1,80 @@
 local transpiler = require("src.transpiler")
 
+local function is_windows()
+    return package.config:sub(1,1) == "\\"
+end
+
 local function file_exists(name)
     local f = io.open(name, "r")
-    if f then
-        f:close()
-        return true
-    end
+    if f then f:close() return true end
     return false
 end
 
-local function get_output_name(input)
-    return input:gsub("%.hc$", ".c")
+local function delete_file(path)
+    os.remove(path)
 end
 
 local function compile_and_run(c_file)
     local exe_name = c_file:gsub("%.c$", "")
-    local is_windows = package.config:sub(1,1) == "\\"
+    local win = is_windows()
 
-    print("Compiling with GCC...")
-
-    if is_windows then
-        -- Converte / para \
-        local exe_path = exe_name:gsub("/", "\\") .. ".exe"
-        local c_path = c_file:gsub("/", "\\")
-
-        os.execute('gcc "' .. c_path .. '" -o "' .. exe_path .. '"')
-
-        print("Running executable...")
-        os.execute('cmd /c "' .. exe_path .. '"')
+    if win then
+        local exe_path = exe_name .. ".exe"
+        os.execute('gcc "' .. c_file .. '" -o "' .. exe_path .. '"')
+        os.execute('cmd /c ".\\' .. exe_path:gsub("/", "\\") .. '"')
+        delete_file(exe_path)
     else
         os.execute('gcc "' .. c_file .. '" -o "' .. exe_name .. '"')
-        print("Running executable...")
         os.execute('./' .. exe_name)
+        delete_file(exe_name)
     end
 end
 
--- CLI Arguments
-local input = arg[1]
-local flag = arg[2]
+local function run_single(file)
+    print("\n==============================")
+    print("Running: " .. file)
+    print("==============================")
 
-if not input then
-    print("Usage: lua bin/hc.lua <file.hc> [--run]")
-    return
-end
+    local output = file:gsub("%.hc$", ".c")
 
-if not file_exists(input) then
-    print("Error: File not found -> " .. input)
-    return
-end
-
-local output = get_output_name(input)
-
-print("Transpiling " .. input .. " -> " .. output)
-transpiler.transpile(input, output)
-
-print("Done.")
-
-if flag == "--run" then
+    transpiler.transpile(file, output)
     compile_and_run(output)
+    delete_file(output)
+end
+
+local function run_all_examples()
+    local win = is_windows()
+    local command
+
+    if win then
+        command = 'dir /b examples\\*.hc'
+    else
+        command = 'ls examples/*.hc'
+    end
+
+    local handle = io.popen(command)
+    if not handle then
+        print("Could not list example files.")
+        return
+    end
+
+    for file in handle:lines() do
+        local full_path = "examples/" .. file
+        run_single(full_path)
+    end
+
+    handle:close()
+end
+
+-- CLI
+local arg1 = arg[1]
+
+if arg1 == "--all" then
+    run_all_examples()
+elseif arg1 and file_exists(arg1) then
+    run_single(arg1)
+else
+    print("Usage:")
+    print("  lua bin/hc.lua <file.hc>")
+    print("  lua bin/hc.lua --all")
 end
